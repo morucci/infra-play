a485237af7e1018fa994ab02d45fa1fb8af1a64b
Fabien Boucher <fabien.boucher@enovance.com>
2015-03-26 12:24:22 +0000

    Generate more keys for gerrit

--- prepare-hieradata.sh
+++ prepare-hieradata.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj "/C=FR/O=Somewhere/CN=test" -keyout /tmp/fakekey.key -out /tmp/fakecert.pem
-rm /tmp/fakesshkey
-ssh-keygen -P "" -f /tmp/fakesshkey
+rm /tmp/fakersasshkey
+rm /tmp/fakedsasshkey
+ssh-keygen -t rsa -P "" -f /tmp/fakersasshkey
+ssh-keygen -t dsa -P "" -f /tmp/fakedsasshkey
 
 # Will create /var/lib/hiera/defaults.yaml
 ./sub.py
--- sub.py
+++ sub.py
@@ -6,12 +6,18 @@ dest="/tmp/defaults.yaml"
 
 fakecert = "".join([" %s" % l for l in file('/tmp/fakecert.pem').readlines()]).strip()
 fakekey = "".join([" %s" % l for l in file('/tmp/fakekey.key').readlines()]).strip()
-fakesshkey = "".join([" %s" % l for l in file('/tmp/fakesshkey').readlines()]).strip()
+fakersasshkey = "".join([" %s" % l for l in file('/tmp/fakersasshkey').readlines()]).strip()
+fakersasshkeypub = "".join([" %s" % l for l in file('/tmp/fakersasshkey.pub').readlines()]).strip()
+fakedsasshkey = "".join([" %s" % l for l in file('/tmp/fakedsasshkey').readlines()]).strip()
+fakedsasshkeypub = "".join([" %s" % l for l in file('/tmp/fakedsasshkey.pub').readlines()]).strip()
 fakepass = "wxcvbn"
 
 a = {'fakecert': fakecert,
      'fakekey': fakekey,
-     'fakesshkey': fakesshkey,
+     'fakersasshkey': fakersasshkey,
+     'fakersasshkeypub': fakersasshkeypub,
+     'fakedsasshkey': fakedsasshkey,
+     'fakedsasshkeypub': fakedsasshkeypub,
      'fakepass': fakepass}
 
 s = Template(file('defaults.yaml.tmpl').read())
